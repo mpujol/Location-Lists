17a8165aa9904d61c59a6e02eb1d645d249c7db1
Michael Andrew Pujol <mike.pujol88@gmail.com>
2017-09-18 15:09:16 -0700

    Initial Project Setup

---
+++ LocationLists/LocationLists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationLists
+//
+//  Created by Michael Pujol on 9/18/17.
+//  Copyright © 2017 Michael Pujol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
